9fb8172f45d7e5a55dd428dde2a6d72c58296f05
andrew <andrew.g.warrdobe@googlemail.com>
2015-03-22 20:56:20 +0000

    Updating Data

--- data/testData.sql
+++ data/testData.sql
@@ -4,14 +4,20 @@ insert into "biography" values (3, 'DJ D-Lux', 0, 'images/crew/dlux_col.png', 'i
 insert into "biography" values (4, 'T-Bag', 0, 'images/crew/tbag_col.png', 'images/crew/tbag_col.png','blah blah');
 insert into "biography" values (5, 'Pope Frost', 0, 'images/crew/popefrost_col.png', 'images/crew/popefrost_col.png','blah blah');
 insert into "biography" values (6, 'Jean Jemonge', 0, 'images/crew/jeanjemonge_col.png', 'images/crew/jeanjemonge_col.png','blah blah');
-insert into "biography" values (7, 'MC Donalds', 0, 'images/crew/donalds_bw.jpg', 'images/crew/donalds_bw.jpg','blah blah');
-insert into "biography" values (8, 'MC Donalds', 0, 'images/crew/donalds_bw.jpg', 'images/crew/donalds_bw.jpg','blah blah');
-insert into "biography" values (9, 'MC Donalds', 0, 'images/crew/donalds_bw.jpg', 'images/crew/donalds_bw.jpg','blah blah');
-insert into "biography" values (10, 'MC Donalds', 0, 'images/crew/donalds_bw.jpg', 'images/crew/donalds_bw.jpg','blah blah');
-insert into "biography" values (11, 'MC Donalds', 0, 'images/crew/donalds_bw.jpg', 'images/crew/donalds_bw.jpg','blah blah');
-insert into "biography" values (12, 'MC Donalds', 0, 'images/crew/donalds_bw.jpg', 'images/crew/donalds_bw.jpg','blah blah');
+insert into "biography" values (7, 'D-T-R', 0, 'images/crew/dtr_col.png', 'images/crew/dtr_col.png','blah blah');
+insert into "biography" values (8, 'Maxiam Baud Laveine', 0, 'images/crew/mbl_col.png', 'images/crew/mbl_col.png','blah blah');
+insert into "biography" values (9, 'Moist Conditioner', 0, 'images/crew/moist_col.png', 'images/crew/moist_col.png','blah blah');
+
+
+insert into "biography" values (12, 'Gaz Three', 0, 'images/crew/gazthree_col.png', 'images/crew/gazthree_col.png','blah blah');
+insert into "biography" values (11, 'Josep Jorg', 0, 'images/crew/jorg_col.png', 'images/crew/jorg_col.png','blah blah');
+insert into "biography" values (10, 'Hans Pettersonn', 0, 'images/crew/hans_col.png', 'images/crew/hans_col.png','blah blah');
 insert into "biography" values (13, 'MC Donalds', 0, 'images/crew/donalds_bw.jpg', 'images/crew/donalds_bw.jpg','blah blah');
 insert into "biography" values (14, 'MC Donalds', 0, 'images/crew/donalds_bw.jpg', 'images/crew/donalds_bw.jpg','blah blah');
+
+
+
+insert into "biography" values (88, 'Hitler', 0, 'images/crew/hitler_col.png', 'images/crew/hitler_col.png','Hitler is a very naughty man');
 insert into "blog" values (1, 'Test Post',1, {ts '2012-09-17 18:47:52.69'}, 'MC Donalds', 'Blah Blah');
 insert into "blog" values (2, 'Test Post 1',1, {ts '2012-09-17 18:47:52.69'}, 'MC Donalds', 'Blah Blah <div>Blah</div> <script></script> <a href="#">leek</a> Blah');
 insert into "blog" values (3, 'Test Post 2',1, {ts '2012-09-17 18:47:52.69'}, 'MC Donalds', 'Blah Blah');
