a349da1edead8274a273c4bafb983c8ba7ed7ae2
andrew <andrew.g.warrdobe@googlemail.com>
2015-03-23 22:03:48 +0000

    Added the Skall and X Smokes logo as a background image

---
+++ test/test/unit/JsonApiSpec.scala
@@ -0,0 +1,22 @@
+package test.unit
+
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+/**
+ * Created by andrew on 14/09/14.
+ */
+class JsonApiSpec extends PlaySpec with OneServerPerSuite{
+
+  "Json API" should {
+
+    "be able to list the number of track on a relase" in (pending)
+
+
+    def setup = {
+
+    }
+  }
+
+}
