6d7690b090d29ffb1cdaf80f70ca030b0878e280
andrew <andrew.g.warrdobe@googlemail.com>
2015-04-02 20:18:07 +0100

    Added Additional user roles

--- app/controllers/AuthConfiglmpl.scala
+++ app/controllers/AuthConfiglmpl.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import jp.t2v.lab.play2.auth.AuthConfig
-import models.UserRole.{NormalUser, Administrator}
+import models.UserRole.{InActiveUser, Contributor, NormalUser, Administrator}
 import models.{UserRole, UserAccount}
 import play.api.db.DB
 import play.api.db.slick.DBAction
@@ -69,7 +69,10 @@ trait AuthConfigImpl extends AuthConfig {
   def authorize(user: User, authority: Authority)(implicit ctx: ExecutionContext): Future[Boolean] = Future.successful {
     (user.role, authority) match {
       case (Administrator, _)       => true
-      case (NormalUser, NormalUser) => true
+      case(Contributor,Contributor) => true
+      case(Contributor,NormalUser) => true
+      case(NormalUser, NormalUser) => true
+      case(InActiveUser, InActiveUser) => true
       case _                        => false
     }
   }
